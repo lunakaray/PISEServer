243e4ea093c86b06f16764be1e389dbecdbc2615
lunakaray <93786059+lunakaray@users.noreply.github.com>
2023-01-13 14:36:27 +0200

    changed toy example to match the maat execution code

--- examples/toy_example/toy_client_inference.py
+++ examples/toy_example/toy_client_inference.py
@@ -1,43 +1,53 @@
 import logging
 
-from pise import sym_execution, server, hooks
+from maat import *
+# from typing import List
+from pise import sym_execution_maat, server
 
 
-class ToySendHook(hooks.SendReceiveCallSite):
-
-    def get_return_value(self, buff, length, call_context):
-        # Something messed up with angr return value handling, so we simply set rax with the desired return value
-        call_context.state.regs.rax = length
-
-    def set_hook(self, p):
-        p.hook_symbol('send', hooks.SendHook(self))
-
-    def extract_arguments(self, call_context):
-        length = call_context.state.regs.edx
-        buffer = call_context.state.regs.rsi
-        return buffer, length
-
-
-class ToyRecvHook(hooks.SendReceiveCallSite):
+class ToySendHook:
+    def __init__(self, send_msg_add):
+        self.send_msg_add = send_msg_add
 
     def get_return_value(self, buff, length, call_context):
         # Something messed up with angr return value handling, so we simply set rax with the desired return value
-        call_context.state.regs.rax = length
+        # call_context.state.regs.rax = length
+        return
 
-    def set_hook(self, p):
-        p.hook_symbol('recv', hooks.RecvHook(self))
+    def set_hook(self, m: MaatEngine):
+        m.hooks.add(EVENT.EXEC, WHEN.BEFORE, name="send", filter=self.send_msg_add)
 
     def extract_arguments(self, call_context):
-        length = call_context.state.regs.edx
-        buffer = call_context.state.regs.rsi
-        return buffer, length
+        # length = call_context.state.regs.edx
+        # buffer = call_context.state.regs.rsi
+        #return buffer, length
+        return
+
+
+class ToyRecvHook:
+    def __init__(self, recv_msg_add):
+        self.recv_msg_add = recv_msg_add
+
+    def get_return_value(self, buff, length, call_context):
+        # Something messed up with angr return value handling, so we simply set rax with the desired return value
+        # call_context.state.regs.rax = length
+        return
+
+    def set_hook(self, m: MaatEngine):
+        m.hooks.add(EVENT.EXEC, WHEN.BEFORE, name="recv", filter=self.recv_msg_add)
+
+    def extract_arguments(self, call_context):
+        # length = call_context.state.regs.edx
+        # buffer = call_context.state.regs.rsi
+        # return buffer, length
+        return
 
 
 def main():
     logging.getLogger('pise').setLevel(logging.DEBUG)
     # logging.getLogger('angr').setLevel(logging.INFO)
-    query_runner = sym_execution.QueryRunner('examples/toy_example/toy_example', [ToySendHook(), ToyRecvHook()])
-    s = server.Server(query_runner)
+    query_runner = sym_execution_maat.QueryRunner('examples/toy_example/toy_example', [ToySendHook(0x61b), ToyRecvHook(0x627), 0x61b, 0x627])
+    s = server.Server(query_runner=query_runner)
     s.listen()
 
 
